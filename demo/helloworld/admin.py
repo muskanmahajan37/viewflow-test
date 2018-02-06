c5c1830f4b5bc9496f52a70c85daa857c61cde0f
Phodal HUANG <h@phodal.com>
2018-02-06 11:31:17 +0800

    [78] update config

--- demo/helloworld/admin.py
+++ demo/helloworld/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from viewflow.admin import ProcessAdmin
 
-# Register your models here.
+from . import models
+
+
+class HelloWorldProcessAdmin(ProcessAdmin):
+    icon = '<i class="material-icons">flag</i>'
+    list_display = ['pk', 'created', 'status', 'participants',
+                    'text', 'approved']
+    list_display_links = ['pk', 'created']
+
+
+admin.site.register(models.HelloWorldProcess, HelloWorldProcessAdmin)
--- demo/helloworld/apps.py
+++ demo/helloworld/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class HelloworldConfig(AppConfig):
+    icon = '<i class="material-icons">flag</i>'
     name = 'helloworld'
